e09dcbd61eec90c8fbc103e3c1b736f0ab5945ab
Diego Doná <diedona@gmail.com>
2019-11-13 10:41:24 -0300

    add precoproduto @ domain

---
+++ DDona.BackingFieldEF3.Domain/Entities/PrecoProduto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.BackingFieldEF3.Domain.Entities
+{
+    public class PrecoProduto
+    {
+        public Guid Id { get; set; }
+        public Guid IdProduto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime Data { get; set; }
+        public bool Ativo { get; set; }
+
+        public Produto Produto { get; set; }
+    }
+}
--- DDona.BackingFieldEF3.Domain/Entities/Produto.cs
+++ DDona.BackingFieldEF3.Domain/Entities/Produto.cs
@@ -6,9 +6,18 @@ namespace DDona.BackingFieldEF3.Domain.Entities
 {
     public class Produto
     {
+        private readonly List<PrecoProduto> _precoProdutos;
+
         public Guid Id { get; set; }
         public string Nome { get; set; }
         public DateTime DataCriado { get; set; }
         public decimal? PrecoUnidade { get; set; }
+
+        public Produto()
+        {
+            _precoProdutos = new List<PrecoProduto>();
+        }
+
+        public IReadOnlyCollection<PrecoProduto> Precos { get { return _precoProdutos.AsReadOnly(); } }
     }
 }
