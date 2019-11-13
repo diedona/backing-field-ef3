e22369f934ddc8086fb5cc573493899f5bf31692
Diego Doná <diedona@gmail.com>
2019-11-13 10:02:58 -0300

    naming + configuration

---
+++ DDona.BackingFieldEF3.Infra/DbContexts/SistemaContext.cs
@@ -0,0 +1,26 @@
+﻿using DDona.BackingFieldEF3.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.BackingFieldEF3.Infra.DbContexts
+{
+    public class SistemaContext : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+        public SistemaContext() : base() { }
+
+        public SistemaContext(DbContextOptions<SistemaContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // todas as configurações normais de entidade viriam aqui
+
+            modelBuilder.Entity<Produto>().HasKey(x => x.Id);
+
+            // código abreviado por claridade
+        }
+    }
+}
